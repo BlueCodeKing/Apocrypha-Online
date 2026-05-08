3cd6e8155c31e95bc7efd91c6fc3f528e7ffb8a1
BlueCodeKing <105813725+BlueCodeKing@users.noreply.github.com>
2026-05-07 21:54:20 -0500

    Add files via upload

---
+++ scripts/openAndClose.js
@@ -0,0 +1,23 @@
+function closeMenu() {
+    document.getElementById("slidingMenu").style.right = "-300px";
+};
+
+function openMenu() {
+    document.getElementById("slidingMenu").style.right = "0";
+};
+
+function closeMenu2() {
+    document.getElementById("rightSide").style.right = "-300px";
+};
+
+function openMenu2() {
+    document.getElementById("rightSide").style.right = "0";
+};
+
+function closeMenu3() {
+    document.getElementById("leftSide").style.right = "-300px";
+};
+
+function openMenu3() {
+    document.getElementById("leftSide").style.right = "0";
+};
